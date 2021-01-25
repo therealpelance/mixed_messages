fd49e83c5368460ef6e1455644d7cac8be276524
Rob Pelance <rpelance@gmail.com>
2021-01-24 20:09:37 -0500

    add quotes to array

---
+++ script.js
@@ -0,0 +1,18 @@
+let statements = [];
+let quotes = ['Belive in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy. - Norman Vincent Peale', 
+'If you can dream it, you can do it. - Walt Disney', 
+'Where there is a will, there is a way. If there is a chance in a million that you can do something, anything, to keep what you want from ending, do it. Pry the door open or, if need be, wedge your foot in that door and keep it open. — Pauline Kael',
+"Do not wait; the time will never be ‘just right.’ Start where you stand, and work with whatever tools you may have at your command, and better tools will be found as you go along. — George Herbert",
+'Press forward. Do not stop, do not linger in your journey, but strive for the mark set before you. — George Whitefield',
+'The future belongs to those who believe in the beauty of their dreams. — Eleanor Roosevelt',
+'Aim for the moon. If you miss, you may hit a star. — W. Clement Stone',
+"Don’t watch the clock; do what it does. Keep going. — Sam Levenson",
+'There will be obstacles. There will be doubters. There will be mistakes. But with hard work, there are no limits. — Michael Phelps',
+'Keep your eyes on the stars, and your feet on the ground. — Theodore Roosevelt',
+'We aim above the mark to hit the mark. — Ralph Waldo Emerson',
+'One way to keep momentum going is to have constantly greater goals. — Michael Korda',
+"Change your life today. Don’t gamble on the future, act now, without delay. — Simone de Beauvoir",
+"You just can’t beat the person who never gives up. — Babe Ruth",
+'Start where you are. Use what you have. Do what you can. — Arthur Ashe'];
+let challenges = [];
+
