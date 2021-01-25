087a7ff999a7dc1f221217770d334cbb4eb967aa
Rob Pelance <rpelance@gmail.com>
2021-01-24 23:00:10 -0500

    add functions for message creation and display

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Motivational Statement Components
 let statements = ['Today is your day!', 'Your time is now!', 'Tomorrow is a brand new day.', "There's no day like today!", 'No time like the present.', 'Seize the day!', 'Carpe Diem!', "You've got this!", 'The future is in your hands.', 'You have the power.', 'You are a star!', 'The future is yours.', 'You write your own story.', 'The day is yours.', 'The future is here.'];
 let quotes = ['Belive in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy. - Norman Vincent Peale', 
 'If you can dream it, you can do it. - Walt Disney', 
@@ -16,3 +17,27 @@ let quotes = ['Belive in yourself! Have faith in your abilities! Without a humbl
 'Start where you are. Use what you have. Do what you can. — Arthur Ashe'];
 let challenges = ['Write your story today.', 'Take control of your own life.', "Learn from your mistakes and don't repeat them.", 'Each day is a new opportunity to do better than the last.', "Take advantage of each day you're given.", 'Only you can decide how you react to whatever today brings.', 'Control your attitude and you control your life.', "Don't sweat the small stuff.", 'Find your adventure today.', 'Be your own superhero.', 'Create the change you want to see.', "Let go of what doesn't serve you.", 'Refuse to give up until you have tried every option.', 'Make yourself proud today.', 'Do not let your past be a reflection of your future.'];
 
+// Function to choose a random component from an array
+function getComponent(array) {
+	return array[Math.floor(Math.random() * (array.length - 1))]
+};
+
+// Motivational Message Object
+const motivationalMessage = {
+	statement: getComponent(statements),
+	quote: getComponent(quotes),
+	challenge: getComponent(challenges)
+};
+
+// Function to format and display message
+function displayMessage(message) {
+	console.log('------------------------------');
+	console.log(motivationalMessage.statement);
+	console.log('\n');
+	console.log('Your quote of the day:' + '\n' + `     ${motivationalMessage.quote}`);
+	console.log('\n');
+	console.log('Your challenge for the day:' + '\n' + `     ${motivationalMessage.challenge}`);
+	console.log('------------------------------');
+};
+
+displayMessage(motivationalMessage);
